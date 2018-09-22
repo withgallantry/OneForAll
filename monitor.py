83768de9f271392301eed47022514e467b373a38
Matty Gallantry <matt@wakelet.com>
2018-09-22 23:25:56 +0100

    putting back

--- monitor.py
+++ monitor.py
@@ -86,8 +86,8 @@ wifi_2bar = 4
 wifi_3bar = 5
 
 # Joystick Hardware settings
-DZONE = 3300  # dead zone applied to joystick (mV)
-VREF = 5100  # joystick Vcc (mV)
+DZONE = 500  # dead zone applied to joystick (mV)
+VREF = 3300  # joystick Vcc (mV)
 
 # Configure Buttons
 LEFT = 26

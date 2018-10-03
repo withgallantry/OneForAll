bd4448011d830b7e91b30320a74cd29618e180c2
Matty Gallantry <matt@wakelet.com>
2018-10-03 10:52:38 +0100

    Added better debounce

--- monitor.py
+++ monitor.py
@@ -351,6 +351,40 @@ def inputReading():
         time.sleep(.05)
 
 
+class ButtonHandler(threading.Thread):
+    def __init__(self, pin, func, edge='both', bouncetime=200):
+        super().__init__(daemon=True)
+
+        self.edge = edge
+        self.func = func
+        self.pin = pin
+        self.bouncetime = float(bouncetime) / 1000
+
+        self.lastpinval = GPIO.input(self.pin)
+        self.lock = threading.Lock()
+
+    def __call__(self, *args):
+        if not self.lock.acquire(blocking=False):
+            return
+
+        t = threading.Timer(self.bouncetime, self.read, args=args)
+        t.start()
+
+    def read(self, *args):
+        pinval = GPIO.input(self.pin)
+
+        if (
+                    ((pinval == 0 and self.lastpinval == 1) and
+                         (self.edge in ['falling', 'both'])) or
+                    ((pinval == 1 and self.lastpinval == 0) and
+                         (self.edge in ['rising', 'both']))
+        ):
+            self.func(*args)
+
+        self.lastpinval = pinval
+        self.lock.release()
+
+
 def checkKeyInput():
     global info
 
@@ -362,13 +396,14 @@ def checkKeyInput():
         condition.release()
         if not gpio.input(UP):
             volumeUp()
-            time.sleep(BOUNCE_TIME)
+            time.sleep(500)
         elif not gpio.input(DOWN):
             volumeDown()
-            time.sleep(BOUNCE_TIME)
+            time.sleep(500)
         elif not gpio.input(LEFT):
             readModeWifi(True)
-            time.sleep(BOUNCE_TIME)
+            print "Toggle WIFI"
+            time.sleep(500)
 
     if info == True:
         info = False

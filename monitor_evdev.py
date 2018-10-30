290e2f8a35d80f71cdfe9556dcb2d6b805136269
Matty Gallantry <matt@wakelet.com>
2018-10-30 18:25:41 +0000

    Added more

--- monitor_evdev.py
+++ monitor_evdev.py
@@ -115,22 +115,14 @@ gpio.setmode(gpio.BCM)
 gpio.setup(BUTTONS, gpio.IN, pull_up_down=gpio.PUD_UP)
 gpio.setup(SHUTDOWN, gpio.IN, pull_up_down=gpio.PUD_UP)
 
-# BUTTON_A: e.BTN_BASE,  # 'A' button
-# BUTTON_B: e.BTN_BASE2,  # 'B' button
-# BUTTON_X: e.BTN_TL,  # 'X' button
-# BUTTON_Y: e.BTN_TR,  # 'Y' button
-# BUTTON_L1: e.BTN_TOP,  # 'L1' button
-# BUTTON_R1: e.BTN_TOP2,  # 'R1' button
-# SELECT: e.BTN_SELECT,  # 'Select' button
-
 KEYS = {  # EDIT KEYCODES IN THIS TABLE TO YOUR PREFERENCES:
     # See /usr/include/linux/input.h for keycode names
     BUTTON_A: e.BTN_A,  # 'A' button
     BUTTON_B: e.BTN_B,  # 'B' button
     BUTTON_X: e.BTN_X,  # 'X' button
     BUTTON_Y: e.BTN_Y,  # 'Y' button
-    BUTTON_L1: e.BTN_TOP,  # 'L1' button
-    BUTTON_R1: e.BTN_TOP2,  # 'R1' button
+    BUTTON_L1: e.BTN_TL,  # 'L1' button
+    BUTTON_R1: e.BTN_TR,  # 'R1' button
     SELECT: e.BTN_SELECT,  # 'Select' button
     START: e.BTN_START,  # 'Start' button
     UP: e.BTN_DPAD_UP,  # Analog up

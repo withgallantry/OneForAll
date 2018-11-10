3fe750dfc3dbe8a9e63f3c35f8066d250a756e60
Matty Gallantry <matt@wakelet.com>
2018-11-10 11:21:40 +0000

    New scale changes

--- monitor_evdev.py
+++ monitor_evdev.py
@@ -252,11 +252,13 @@ def checkShdn(volt):
 def readVoltage():
     global last_bat_read;
     voltVal = adc.read_adc(0, gain=1);
-    volt = int((float(voltVal) * (4.09 / 2047.0)) * 100)
-    last_bat_read = volt;
+    # volt = int((float(voltVal) * (4.09 / 2047.0)) * 100)
+    volt = int((( voltVal * voltscale * dacres + ( dacmax * 5 ) ) / (( dacres * resdivval ) / resdivmul)))
 
-    if voltVal < 300 or (last_bat_read > 300 and voltVal - last_bat_read > 10):
-        voltVal = last_bat_read;
+    if volt < 300 or (last_bat_read > 300 and volt - last_bat_read > 10):
+        volt = last_bat_read;
+
+    last_bat_read = volt;
 
     return volt
 

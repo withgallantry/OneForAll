622ba157b4260d7c1c1e5a9acdd11062dee3a39c
Matty Gallantry <matt@wakelet.com>
2019-08-06 16:21:32 +0100

    Test

--- monitor_evdev.py
+++ monitor_evdev.py
@@ -123,7 +123,7 @@ if not SHUTDOWN == -1:
 if JOYSTICK_DISABLED == 'False':
     KEYS = {  # EDIT KEYCODES IN THIS TABLE TO YOUR PREFERENCES:
         # See /usr/include/linux/input.h for keycode names
-        BUTTON_A: uinput['BTN_A1'],  # 'A' button Possibly
+        BUTTON_A: uinput['BTN_A'],  # 'A' button Possibly
         BUTTON_B: uinput.BTN_B,  # 'B' button
         BUTTON_X: uinput.BTN_X,  # 'X' button
         BUTTON_Y: uinput.BTN_Y,  # 'Y' button
@@ -141,7 +141,7 @@ if JOYSTICK_DISABLED == 'False':
 else:
     KEYS = {  # EDIT KEYCODES IN THIS TABLE TO YOUR PREFERENCES:
         # See /usr/include/linux/input.h for keycode names
-        BUTTON_A: uinput.KEY_LEFTCTRL,  # 'A' button
+        BUTTON_A: uinput['KEY_LEFTCTRL'],  # 'A' button
         BUTTON_B: uinput.KEY_LEFTALT,  # 'B' button
         BUTTON_X: uinput.KEY_Z,  # 'X' button
         BUTTON_Y: uinput.KEY_X,  # 'Y' button

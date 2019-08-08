19f243cdd2125f613121f1be56fd648ae194dc9d
Matty Gallantry <matt@wakelet.com>
2019-08-08 15:45:02 +0100

    Added combo

--- monitor_evdev.py
+++ monitor_evdev.py
@@ -211,7 +211,7 @@ def handle_button(pin):
             device.emit(KEY_COMBOS[frozenset(COMBO_CURRENT_KEYS)], 1)
             print "Combo start"
     else:
-        if COMBO_CURRENT_KEYS is not None:
+        if LAST_TRIGGERED_COMBO is not None:
             device.emit(LAST_TRIGGERED_COMBO, 0)
             print "Stopped Combo"
 

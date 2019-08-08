59a3d7ea0a7d75ea372cf5e55647f3b98805bcd0
Matty Gallantry <matt@wakelet.com>
2019-08-08 09:19:08 +0100

    Repeated keystates

--- monitor_evdev.py
+++ monitor_evdev.py
@@ -223,12 +223,13 @@ def handle_button(pin):
             device.emit(key, 2)
         else:
             device.emit(key, state)
-        PREVIOUS_KEYSTATES.update({pin: state});
         time.sleep(BOUNCE_TIME)
         device.syn()
         logging.debug("Pin: {}, KeyCode: {}, Event: {}".format(pin, key, 'press' if state else 'release'))
     else:
         checkKeyInputPowerSaving()
+
+    PREVIOUS_KEYSTATES.update({pin: state});
 
 
 def handle_shutdown(pin):

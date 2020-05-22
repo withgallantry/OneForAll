d2bf1ce35e428f130ecee6e67aabf185b8896a2e
Matty Gallantry <matt@wakelet.com>
2020-05-22 15:30:52 +0100

    Make voltval string

--- monitor.py
+++ monitor.py
@@ -334,9 +334,10 @@ def checkShdn(volt):
 # Read voltage
 def readVoltage():
     global last_bat_read;
-    voltVal = adc.read_adc(0, gain=1);
+    print 'trying to read volt'
+    voltVal = adc.read_adc(0, gain=1)
+    print 'voltVal' + str(int(voltVal))
     volt = int((float(voltVal) * (4.09 / 2047.0)) * 100)
-    print 'volt' + int(volt)
 
     if volt < 300 or (last_bat_read > 300 and last_bat_read - volt > 6 and not last_bat_read == 450):
         volt = last_bat_read;
